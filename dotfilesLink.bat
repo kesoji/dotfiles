3a6b9f58e2ae6663776eba494acc8cb3a49019ba
Kenji Sakurada <ten.schwert573@gmail.com>
2016-05-05 18:15:54 +0900

    Create Windows sync .bat file

---
+++ dotfilesLink.bat
@@ -0,0 +1,15 @@
+@echo off
+
+IF NOT EXIST %USERPROFILE%"\.vim" (
+    echo ".vim doesn't exist. creating .vim."
+    mkdir %USERPROFILE%"\.vim"
+) ELSE (
+    echo ".vim exists."
+)
+
+mklink %USERPROFILE%"\.vimrc" %USERPROFILE%"\dotfiles\.vimrc"
+mklink %USERPROFILE%"\.gvimrc" %USERPROFILE%"\dotfiles\.gvimrc"
+mklink %USERPROFILE%"\.vim\mswin.vim" %USERPROFILE%"\dotfiles\mswin.vim"
+mklink /D %USERPROFILE%"\.vim\rc" %USERPROFILE%"\dotfiles\rc"
+mklink /D %USERPROFILE%"\.vim\snippets" %USERPROFILE%"\dotfiles\snippets"
+mklink /D %USERPROFILE%"\vimfiles" %USERPROFILE%"\.vim"
