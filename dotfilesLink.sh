08e32816955dca08cd91106f8569382b79c0ced4
Kenji Sakurada <please_find_me_at_intra@kenjisakurada.jp>
2016-05-19 10:05:36 +0900

    Fix dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+[ -d ~/.vim ] || mkdir ~/.vim
+[ -d ~/.vim/backup ] || mkdir ~/.vim/backup
+[ -d ~/.vim/swp ] || mkdir ~/.vim/swp
+[ -d ~/.vim/undo ] || mkdir ~/.vim/undo
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/rc ~/.vim
 ln -sf ~/dotfiles/snippets ~/.vim
-[ -d ~/.vim/backup ] || mkdir ~/.vim/backup
-[ -d ~/.vim/swp ] || mkdir ~/.vim/swp
-[ -d ~/.vim/undo ] || mkdir ~/.vim/undo
