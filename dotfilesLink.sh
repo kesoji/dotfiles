cbdd5a0f08a8323b2a2dc27913503b35a1af15e4
Kenji Sakurada <ten.schwert573@gmail.com>
2016-10-04 00:45:01 +0900

    Modify dotfileslink for .gitignore_global

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,6 +7,8 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+
 ln -sf ~/dotfiles/rc ~/.vim
 ln -sf ~/dotfiles/snippets ~/.vim
 ln -sf ~/dotfiles/after ~/.vim
