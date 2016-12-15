e746f2418c5245e80a8756f6fae32fbc1d61c7e1
Kenji Sakurada <ten.schwert573@gmail.com>
2016-12-16 03:38:06 +0900

    Fix dotfileslink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/local/bin/zsh
 [ -d ~/.vim ] || mkdir ~/.vim
 [ -d ~/.vim/backup ] || mkdir ~/.vim/backup
 [ -d ~/.vim/swp ] || mkdir ~/.vim/swp
@@ -7,10 +7,18 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
+# dir
 ln -sf ~/dotfiles/rc ~/.vim
 ln -sf ~/dotfiles/snippets ~/.vim
 ln -sf ~/dotfiles/after ~/.vim
+
+# zplezto
+[ -d ~/.zprezto ] || git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
