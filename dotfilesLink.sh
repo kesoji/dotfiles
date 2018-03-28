fc498befd0ca58a57dec855b5c545bf999c14316
Kenji Sakurada <ten.schwert573@gmail.com>
2018-03-28 22:17:09 +0900

    split

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,16 +3,16 @@
 [ -d ~/.vim/backup ] || mkdir ~/.vim/backup
 [ -d ~/.vim/swp ] || mkdir ~/.vim/swp
 [ -d ~/.vim/undo ] || mkdir ~/.vim/undo
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.zshenv ~/.zshenv
-ln -sf ~/dotfiles/.sshrc ~/.sshrc
-ln -sf ~/dotfiles/.tigrc ~/.tigrc
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/ideavimrc ~/.ideavimrc
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/gitignore_global ~/.gitignore_global
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/zshenv ~/.zshenv
+ln -sf ~/dotfiles/sshrc ~/.sshrc
+ln -sf ~/dotfiles/tigrc ~/.tigrc
 
 # dir
 ln -sf ~/dotfiles/rc ~/.vim
@@ -20,12 +20,12 @@ ln -sf ~/dotfiles/snippets ~/.vim
 ln -sf ~/dotfiles/after ~/.vim
 ln -sf ~/dotfiles/memo ~/.vim
 ln -sf ~/dotfiles/template ~/.vim
-ln -sf ~/dotfiles/.sshrc.d ~/
+ln -sf ~/dotfiles/sshrc.d ~/.sshrc.d
 
 # zplezto
 [ -d ~/.zprezto ] || git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 for rcfile in ${ZDOTDIR:-$HOME}/.zprezto/runcoms/z*; do
     filename=`basename $rcfile`
-    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.$filename"
+    ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.$filename"
 done
-ln -sf ~/dotfiles/.zpreztorc ~/.zpreztorc
+ln -sf ~/dotfiles/zpreztorc ~/.zpreztorc
