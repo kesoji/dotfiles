6b577457e02e5922fc908a5d8c6e37d6700cee03
Kenji Sakurada <ten.schwert573@gmail.com>
2018-04-30 00:57:06 +0900

    add vim-plug auto install

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -30,3 +30,10 @@ ln -sf ~/dotfiles/template ~/.vim/
 #    ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.$filename"
 #done
 ln -sf ~/dotfiles/zpreztorc ~/.zpreztorc
+
+# vim-plug
+if [ ! -s ~/.vim/autoload/plug.vim ] ; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
