c75613b2d8412ef1178a0e44760a66c7b77591d8
Kenji Sakurada <ten.schwert573@gmail.com>
2018-04-29 23:38:10 +0900

    fix

--- dotfilesLink.bat
+++ dotfilesLink.bat
@@ -15,7 +15,7 @@ mklink %USERPROFILE%"\.tmux.conf" %USERPROFILE%"\dotfiles\tmux.conf"
 mklink %USERPROFILE%"\.gitconfig" %USERPROFILE%"\dotfiles\gitconfig"
 mklink %USERPROFILE%"\.gitignore_global" %USERPROFILE%"\dotfiles\gitignore_global"
 
-mklink %USERPROFILE%"\vim\mswin.vim" %USERPROFILE%"\dotfiles\mswin.vim"
+mklink %USERPROFILE%"\.vim\mswin.vim" %USERPROFILE%"\dotfiles\mswin.vim"
 
 mklink %USERPROFILE%"\.zshrc" %USERPROFILE%"\dotfiles\zshrc"
 mklink %USERPROFILE%"\.zshenv" %USERPROFILE%"\dotfiles\zshenv"
