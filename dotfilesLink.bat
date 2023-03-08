dfb0ea3cf8276cdd71738384f0c02e12b6de8e89
kesoji <6465251+kesoji@users.noreply.github.com>
2023-03-08 21:08:30 +0900

    update windows script

--- dotfilesLink.bat
+++ dotfilesLink.bat
@@ -11,17 +11,7 @@ mklink %USERPROFILE%"\.vimrc" %USERPROFILE%"\dotfiles\vimrc"
 mklink %USERPROFILE%"\.gvimrc" %USERPROFILE%"\dotfiles\gvimrc"
 mklink %USERPROFILE%"\.ideavimrc" %USERPROFILE%"\dotfiles\ideavimrc"
 mklink %USERPROFILE%"\.vsvimrc" %USERPROFILE%"\dotfiles\vsvimrc"
-mklink %USERPROFILE%"\.tmux.conf" %USERPROFILE%"\dotfiles\tmux.conf"
-mklink %USERPROFILE%"\.gitconfig" %USERPROFILE%"\dotfiles\gitconfig"
-mklink %USERPROFILE%"\.gitignore_global" %USERPROFILE%"\dotfiles\gitignore_global"
-
 mklink %USERPROFILE%"\.vim\mswin.vim" %USERPROFILE%"\dotfiles\mswin.vim"
-
-mklink %USERPROFILE%"\.zshrc" %USERPROFILE%"\dotfiles\zshrc"
-mklink %USERPROFILE%"\.zshenv" %USERPROFILE%"\dotfiles\zshenv"
-mklink %USERPROFILE%"\.sshrc" %USERPROFILE%"\dotfiles\sshrc"
-mklink %USERPROFILE%"\.minttyrc" %USERPROFILE%"\dotfiles\minttyrc"
-
 mklink /D %USERPROFILE%"\.vim\rc" %USERPROFILE%"\dotfiles\rc"
 mklink /D %USERPROFILE%"\.vim\snippets" %USERPROFILE%"\dotfiles\snippets"
 mklink /D %USERPROFILE%"\.vim\after" %USERPROFILE%"\dotfiles\after"
@@ -29,3 +19,7 @@ mklink /D %USERPROFILE%"\.vim\memo" %USERPROFILE%"\dotfiles\memo"
 mklink /D %USERPROFILE%"\.vim\template" %USERPROFILE%"\dotfiles\template"
 mklink /D %USERPROFILE%"\.vim\config" %USERPROFILE%"\dotfiles\vim"
 mklink /D %USERPROFILE%"\vimfiles" %USERPROFILE%"\.vim"
+
+mklink %USERPROFILE%"\.gitconfig" %USERPROFILE%"\dotfiles\gitconfig"
+mklink %USERPROFILE%"\.gitignore_global" %USERPROFILE%"\dotfiles\gitignore_global"
+
