f194a3c66071e266926da787f45f29871b3544c4
kesoji <6465251+kesoji@users.noreply.github.com>
2024-01-14 14:13:09 +0900

    nvim plugin manager + smoothcursor

---
+++ nvim/init.lua
@@ -0,0 +1,28 @@
+-- Package Manager - lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup(
+  {
+    { 'gen740/SmoothCursor.nvim', config = function() require('smoothcursor').setup(
+      {
+        fancy = { enable = true },
+      }) end
+    },
+  }, 
+  nil)
+
+
+-- 既存のVim Scriptを読み込む
+vim.opt.rtp:append('~/.vim') -- prependの方がいいのだろうか
+vim.opt.rtp:append('~/.vim/after')
+vim.cmd('source ~/.vimrc')
