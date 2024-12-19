573778ae9337f84ba87bf1208e7040015338e4c0
kesoji <6465251+kesoji@users.noreply.github.com>
2024-12-19 15:17:37 +0900

    fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -63,7 +63,7 @@ else
           {
             'nvim-telescope/telescope.nvim',
             branch = '0.1.x',
-            dependencies = { 'nvim-lua/plenary.nvim' }
+            dependencies = { 'nvim-lua/plenary.nvim' },
             keys = {
               { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
               { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
