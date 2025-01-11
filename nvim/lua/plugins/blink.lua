24a281c7a05c7efa81d2656139b22974735a573a
kesoji <6465251+kesoji@users.noreply.github.com>
2025-01-11 14:42:44 +0900

    auto-save

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,8 +1,10 @@
 return {
-  "saghen/blink.cmp",
-  opts = {
-    keymap = {
-      preset = "super-tab",
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "super-tab",
+      },
     },
   },
 }
