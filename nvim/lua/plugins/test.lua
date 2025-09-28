552e55fda73fa89dec374d26c3efee7c7f336218
kesoji <6465251+kesoji@users.noreply.github.com>
2025-09-28 10:55:17 +0900

    update

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -5,6 +5,18 @@ return {
     opts = {
       adapters = {
         "neotest-vitest",
+        ["neotest-golang"] = {
+          -- Here we can set options for neotest-golang, e.g.
+          -- go_test_args = { "-v", "-race", "-count=1", "-timeout=60s" },
+          dap_go_enabled = true, -- requires leoluz/nvim-dap-go
+          runner = "gotestsum",
+          go_test_args = {
+            "-v",
+            "-race",
+            "-count=1",
+            -- "-coverprofile=" .. vim.fn.getcwd() .. "/coverage.out",
+          },
+        },
       },
     },
   },
